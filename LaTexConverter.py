c03920a75d2cd8ae10f482ad2af1bdda7e125095
Eric Loucks <eloucks@gmail.com>
2018-01-19 11:50:04 +0000

    various incremental updates'

---
+++ LaTexConverter.py
@@ -0,0 +1,8 @@
+# make some data into LaTex Notation
+import coneAngles
+
+print('This program takes an input file of formatted shapes and outputs LaTex Code for all the K differential covering info')
+wait()
+
+def  differentialStrata():
+	
